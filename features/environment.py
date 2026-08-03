32ea5870188f929eacc31f619305723054e870ef
Depy96 <Depy96@users.noreply.github.com>
2026-08-03 14:49:32 -0400

    Fix headless Chrome configuration

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@ Environment for Behave Testing
 """
 from os import getenv
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '30'))
 BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
@@ -23,8 +24,10 @@ def before_all(context):
 
 
 def after_all(context):
-    """ Executed after all tests """
-    context.driver.quit()
+    """Executed after all tests"""
+    driver = getattr(context, "driver", None)
+    if driver:
+        driver.quit()
 
 ######################################################################
 # Utility functions to create web drivers
@@ -33,9 +36,16 @@ def after_all(context):
 def get_chrome():
     """Creates a headless Chrome driver"""
     options = webdriver.ChromeOptions()
+    options.binary_location = "/usr/bin/chromium"
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
-    return webdriver.Chrome(options=options)
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
+
+    service = Service("/usr/bin/chromedriver")
+    return webdriver.Chrome(service=service, options=options)
 
 
 def get_firefox():
